b7536a3fd2a1fdfc5072b86ab00b6f8f89af47d2
tbushman <tbushman@pu.bli.sh>
2020-03-21 02:49:59 -0600

    join fail

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const crypto = require('crypto');
 const path = require('path');
 const zoom_key = process.env.clientID;
 const zoom_sec = process.env.clientSecret;
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
 const moment = require('moment');
 const multer = require('multer');
+const rp = require('request-promise');
 const upload = multer({fieldSize: 25 * 1024 * 1024});
 const parseForm = bodyParser.urlencoded({ extended: false });
 const parseJSONBody = bodyParser.json();
@@ -18,7 +21,7 @@ const request = require('request')
 function ensureAdmin(req, res, next) {
 	
 }
-
+// OAuth
 function getAuthCode(req, res, next) {
 	// Check if the code parameter is in the url 
 	// if an authorization code is available, the user has most likely been redirected from Zoom OAuth
@@ -77,16 +80,39 @@ function getAuthCode(req, res, next) {
 
 	}
 }
+// JWT auth
+function getAuthCodeJWT(req, res, next) {
+	const payload = {
+		iss: process.env.JWT_KEY,
+		exp: ((new Date()).getTime() + 5000)
+	};
+	req.token = jwt.sign(payload, process.env.JWT_SECRET);
+	console.log(req.token)
+	return next();
+}
+
+function generateSignature(apiKey, apiSecret, meetingNumber, role) {
+
+	const timestamp = new Date().getTime()
+	const msg = Buffer.from(apiKey + meetingNumber + timestamp + role).toString('base64')
+	const hash = crypto.createHmac('sha256', apiSecret).update(msg).digest('base64')
+	const signature = Buffer.from(`${apiKey}.${meetingNumber}.${timestamp}.${role}.${hash}`).toString('base64')
+
+	return signature
+}
 
 // Helper functions
 
 function deleteMeeting(id, token, next) {
 	
 	const dOptions = {
-		method: 'DELETE',
-		url: `https://api.zoom.us/v2/meetings/${id}`,
+		uri: `https://api.zoom.us/v2/meetings/${id}`,
+		auth: {
+			'bearer': token
+		},
 		headers: {
-			Authorization: 'Bearer ' + token
+			'User-Agent': 'Zoom-Jwt-Request',
+			'content-type': 'application/json'
 		}
 	};
 	request(dOptions, (error, response, data) => {
@@ -176,40 +202,42 @@ router.get('/logout', (req, res, next) => {
 	return res.redirect('/')
 })
 
-router.post('/signature/:meetingnumber', (req, res, next) => {
+router.post('/signature/:meetingnumber', getAuthCodeJWT, (req, res, next) => {
 	const crypto = require('crypto') // crypto comes with Node.js
-	const meetingNumber = req.params.meetingnumber;
-	const role = (req.session.token ? 1 : 0)
-	function generateSignature(apiKey, apiSecret, meetingNumber, role) {
-
-	  const timestamp = new Date().getTime()
-	  const msg = Buffer.from(apiKey + meetingNumber + timestamp + role).toString('base64')
-	  const hash = crypto.createHmac('sha256', apiSecret).update(msg).digest('base64')
-	  const signature = Buffer.from(`${apiKey}.${meetingNumber}.${timestamp}.${role}.${hash}`).toString('base64')
-
-	  return signature
-	}
+	const meetingNumber = 
+		// parseInt(
+		req.params.meetingnumber +''
+		// , 10);
+	const role = (req.token ? 1 : 0);
+	console.log('role')
+	console.log(role)
+	
+	const timestamp = new Date().getTime()
+  const msg = Buffer.from(process.env.JWT_KEY + meetingNumber + timestamp + role).toString('base64')
+  const hash = crypto.createHmac('sha256', process.env.JWT_SECRET).update(msg).digest('base64')
+  const signature = Buffer.from(`${process.env.JWT_KEY}.${meetingNumber}.${timestamp}.${role}.${hash}`).toString('base64')
 	// pass in your Zoom JWT API Key, Zoom JWT API Secret, Zoom Meeting Number, and 0 to join meeting or webinar or 1 to start meeting
-	const signature = generateSignature(process.env.clientID, process.env.clientSecret, meetingNumber, role);
+	// const signature = generateSignature(process.env.JWT_KEY, process.env.JWT_SECRET, meetingNumber, role);
 	// console.log(signature)
-	const key = process.env.clientID;
-	const pw = process.env.WT;
+	const key = process.env.JWT_KEY;
+	// const pw = process.env.WT;
 	const ret = {
 		apiKey: key,
-		wt: pw,
+		// wt: pw,
 		signature: signature
 	}
-	return res.status(200).send(ret)
+	console.log(ret)
+	return res.json(ret)
 })
 
-router.get('/auth', getAuthCode, async (req, res, next) => {
-	if (req.session && req.session.token) {
+router.get('/auth', getAuthCodeJWT, async (req, res, next) => {
+	if (req.token) {
 		// We can now use the access token to authenticate API calls
 		if (!req.session.referrer || /auth/.test(req.session.referrer)) {
 			// Send a request to get your user information using the /me context
 			// The `/me` context restricts an API call to the user the token belongs to
 			// This helps make calls to user-specific endpoints instead of storing the userID
-			getMe(req.session.token, (err, body) => {
+			getMe(req.token, (err, body) => {
 				if (err) {
 					return next(err)
 				} else {
@@ -227,9 +255,9 @@ router.get('/auth', getAuthCode, async (req, res, next) => {
 	}
 })
 
-router.get('/profile', getAuthCode, async (req, res, next) => {
-	if (req.session && req.session.token) {
-		getMe(req.session.token, (err, body) => {
+router.get('/profile', getAuthCodeJWT, async (req, res, next) => {
+	if (req.token) {
+		getMe(req.token, (err, body) => {
 			if (err) {
 				return next(err)
 			} else {
@@ -241,44 +269,55 @@ router.get('/profile', getAuthCode, async (req, res, next) => {
 	}
 })
 
-router.get('/api/createMeeting', getAuthCode, csrfProtection, function(req, res) {
+router.get('/api/createMeeting', getAuthCodeJWT, csrfProtection, function(req, res) {
 	res.render('edit', {
 		csrfToken: req.csrfToken(),
 		title: 'Manage Meetings'
 	});
 });
 
-router.post('/api/createMeeting', upload.array(), parseBody, csrfProtection, function(req, res, next) {
-	// console.log(req.body);
+router.post('/api/createMeeting', getAuthCodeJWT, upload.array(), parseBody, csrfProtection, async function(req, res, next) {
+	console.log(req.body);
 	console.log("topic:", req.body.topic);
 	console.log("agenda:", '# ' +req.body.title + '  \n' + req.body.description);
-	if (req.session && req.session.token) {
-		getMe(req.session.token, (err, body) => {
-			if (err) {
-				return next(err)
+	if (req.token) {
+	// if (req.session && req.session.token) {
+		console.log(req.token)
+		const mOptions = {
+			method: 'POST',
+			uri: `https://api.zoom.us/v2/users/me/meetings`,
+			json: {
+				'topic': req.body.topic,
+				'type': 2,
+				'agenda': '# ' +req.body.title + '  \n' + req.body.description,
+				'start-time': moment().add(5, 'minutes').utc().format(),
+				'duration': 30,
+				'settings': {
+					'host_video': true,
+					'participant_video': true,
+					'join_before_host': true,
+					'mute_upon_entry': true,
+					'enforce_login': false,
+					'meeting_authentication': false,
+					'authentication_domains': '*'
+				}
+			},
+			headers: {
+				Authorization: 'Bearer ' + req.token
 			}
-			const mOptions = {
-				method: 'POST',
-				url: `https://api.zoom.us/v2/users/me/meetings`,
-				json: {
-					'topic': req.body.topic,
-					'agenda': '# ' +req.body.title + '  \n' + req.body.description,
-					'start-time': moment().add(1, 'hours').utc().format(),
-					'duration': 30
-				},
-				headers: {
-					Authorization: 'Bearer ' + req.session.token
-				}
-			};
-			request(mOptions, (error, response, data) => {
-				if (error) {
-					return next(error)
-				} else {
-					// console.log(data)
-					return res.redirect('/meetings')
-				}
-			})
-		})
+			
+		};
+		request(mOptions, (error, response, body) => {
+			if (error) {
+				return next(error)
+			}
+			return res.redirect('/meetings')
+		}) 
+		
+		// ).then(response => {
+		// 	console.log(response)
+		// 	return res.redirect('/meetings')
+		// }).catch(err => console.log(err));
 		
 	} else {
 		// console.log(req.session)
@@ -287,12 +326,12 @@ router.post('/api/createMeeting', upload.array(), parseBody, csrfProtection, fun
 	 
 });
 
-router.get('/meetings', getAuthCode, function(req, res, next) {
+router.get('/meetings', getAuthCodeJWT, function(req, res, next) {
 	const options = {
 		method: 'GET',
 		url: `https://api.zoom.us/v2/users/me/meetings`,
 		headers: {
-			Authorization: 'Bearer ' + req.session.token
+			Authorization: 'Bearer ' + req.token
 		}
 	}
 	// console.log(req.session)
@@ -309,13 +348,13 @@ router.get('/meetings', getAuthCode, function(req, res, next) {
 	})
 });
 
-router.get('/meeting/:id', getAuthCode, (req, res, next) => {
+router.get('/meeting/:id', getAuthCodeJWT, (req, res, next) => {
 	const meetingId = req.params.id;
 	const mOptions = {
 		method: 'GET',
 		url: `https://api.zoom.us/v2/meetings/${meetingId}`,
 		headers: {
-			Authorization: 'Bearer ' + req.session.token
+			Authorization: 'Bearer ' + req.token
 		}
 	};
 	request(mOptions, (error, response, data) => {
@@ -330,44 +369,44 @@ router.get('/meeting/:id', getAuthCode, (req, res, next) => {
 	})
 })
 
-router.get('/group', getAuthCode, (req, res, next) => {
-	const options = {
-		method: 'GET',
-		url: `https://api.zoom.us/v2/groups`,
-		headers: {
-			Authorization: 'Bearer ' + req.session.token
-		}
-	}
-	// console.log(req.session)
-	request(options, async (error, response, groups) => {
-		if (error) {
-			console.log('API Response Error: ', error)
-		} else {
-			console.log(groups)
-			groups = JSON.parse(groups);
-			const grp = await groups.groups.map(group => {
-				const uOptions = {
-					method: 'GET',
-					url: `https://api.zoom.us/v2/groups/${group.id}/members`,
-					headers: {
-						Authorization: 'Bearer ' + req.session.token
-					}
-				}
-				request(uOptions, (err, response, members) => {
-					members = JSON.parse(members).members;
-					group.members = members;
-					return group;
-				})
-			})
-			return res.render('users', {
-				data: grp
-			})
-		}
-	})
-})
+// router.get('/group', getAuthCodeJWT, (req, res, next) => {
+// 	const options = {
+// 		method: 'GET',
+// 		url: `https://api.zoom.us/v2/groups`,
+// 		headers: {
+// 			Authorization: 'Bearer ' + req.session.token
+// 		}
+// 	}
+// 	// console.log(req.session)
+// 	request(options, async (error, response, groups) => {
+// 		if (error) {
+// 			console.log('API Response Error: ', error)
+// 		} else {
+// 			console.log(groups)
+// 			groups = JSON.parse(groups);
+// 			const grp = await groups.groups.map(group => {
+// 				const uOptions = {
+// 					method: 'GET',
+// 					url: `https://api.zoom.us/v2/groups/${group.id}/members`,
+// 					headers: {
+// 						Authorization: 'Bearer ' + req.session.token
+// 					}
+// 				}
+// 				request(uOptions, (err, response, members) => {
+// 					members = JSON.parse(members).members;
+// 					group.members = members;
+// 					return group;
+// 				})
+// 			})
+// 			return res.render('users', {
+// 				data: grp
+// 			})
+// 		}
+// 	})
+// })
 
 // add group members
-// router.post('/group/:id', getAuthCode, (req, res, next) => {
+// router.post('/group/:id', getAuthCodeJWT, (req, res, next) => {
 // 	{
 //   "members": [
 //     {
@@ -387,7 +426,7 @@ router.get('/meetingEnd/:id', (req, res, next) => {
 			'action': 'end'
 		},
 		headers: {
-			Authorization: 'Bearer ' + req.session.token
+			Authorization: 'Bearer ' + req.token
 		}
 	};
 	request(mOptions, (error, response, data) => {
@@ -400,9 +439,9 @@ router.get('/meetingEnd/:id', (req, res, next) => {
 	})
 })
 
-router.get('/api/deleteMeeting/:id', getAuthCode, (req, res, next) => {
+router.get('/api/deleteMeeting/:id', getAuthCodeJWT, (req, res, next) => {
 	// console.log(req.params.id, req.session.token)
-	deleteMeeting(req.params.id, req.session.token, (err) => {
+	deleteMeeting(req.params.id, req.token, (err) => {
 		if (err) {
 			return next(err)
 		}
